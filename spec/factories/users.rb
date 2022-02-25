d2dafd106187067692587f79139ac9c22e206d5c
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-24 18:02:54 -0600

    User model testing

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email { "#{name.downcase.gsub(/ /, "_")}@email.com" }
+    password { "password123" }
+    password_confirmation { "password123" }
+    zip { "80918" }
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password }
+    it { should validate_presence_of(:zip) }
+  end
+
+  describe 'secure user creation' do
+    it 'saves user auth info securely' do
+      user = User.create(name: "Jeff", email: "jeff@email.com", password: "password123", password_confirmation: "password123", zip: "80918")
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq("password123")
+    end
+  end
+end

6bc67ce1d28f88335db3b9cddd9354c580cb90d8
Joel Grant <joelmgrant@me.com>
2022-02-26 15:38:18 -0700

    Create initial testing to construct api consumption

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe WeatherFacade do
   describe '#all_weather' do
     it 'returns weather from the one call endpoint' do
-      
+      weather = WeatherFacade.new
+
+      data = weather.all_weather(80125)
     end
   end
 end

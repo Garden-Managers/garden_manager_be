0db55eb2bdca422cf3c2568172bfde1d926bc9b0
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-26 11:17:26 -0600

    controller action for user update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,36 @@
 class Api::V1::UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def index
     render json: UserSerializer.new(User.all)
   end
 
   def show
-    render json: UserSerializer.new(User.find_by email: params[:email])
+    render json: UserSerializer.new(User.find_by! email: params[:email])
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: UserSerializer.new(user)
+    else
+      not_found
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest, :zip)
+  end
+
+  def not_found
+    payload = {
+      message: "User not found",
+      code: 404,
+      status: "NOT FOUND"
+    }
+    render json: payload, status: 404
   end
 end

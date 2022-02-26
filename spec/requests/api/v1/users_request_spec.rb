cc04f448dfffd7542b341b55c3a0cd10a3024a0f
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-26 11:16:44 -0600

    test for user patch request

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -70,9 +70,53 @@ RSpec.describe 'Users API endpoints' do
       expect(user1[:data][:attributes][:zip]).to be_a(String)
     end
 
-    xit 'returns status 404 if user email invalid' do
-      get '/api/v1/users/peacemaker@aol.com'
+    it 'returns status 404 if user email invalid' do
+      get '/api/v1/user', params: { email: 'peacemaker@aol.com' }
       expect(response.status).to eq(404)
     end
   end
+
+  describe 'PATCH user' do
+    before(:each) do
+      user1 = create(:user)
+      @previous_name = user1.name
+      patch "/api/v1/users/#{user1.id}", params: { user: { name: 'Jenna' } }
+    end
+    let!(:user1) { JSON.parse(response.body, symbolize_names: true) }
+
+    it 'returns successful' do
+      expect(response).to be_successful
+    end
+
+    it 'returns correct info' do
+      expect(user1).to have_key(:data)
+      expect(user1[:data]).to be_a(Hash)
+
+      expect(user1[:data]).to have_key(:id)
+      expect(user1[:data][:id]).to be_a(String)
+
+      expect(user1[:data]).to have_key(:type)
+      expect(user1[:data][:type]).to eq('user')
+
+      expect(user1[:data]).to have_key(:attributes)
+      expect(user1[:data][:attributes]).to be_a(Hash)
+
+      expect(user1[:data][:attributes]).to have_key(:name)
+      expect(user1[:data][:attributes][:name]).to be_a(String)
+      expect(user1[:data][:attributes][:name]).to_not eq(@previous_name)
+
+      expect(user1[:data][:attributes]).to have_key(:email)
+      expect(user1[:data][:attributes][:email]).to be_a(String)
+
+      expect(user1[:data][:attributes]).to have_key(:zip)
+      expect(user1[:data][:attributes][:zip]).to be_a(String)
+    end
+
+    it 'returns status 404 if user not found' do
+      patch '/api/v1/users/6514980891', params: { user: { name: 'Jenna' } }
+
+      expect(response.status).to eq(404)
+    end
+  end
+
 end

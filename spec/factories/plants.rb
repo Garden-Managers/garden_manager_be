49b7792c729a2d27ce6b52bf51bf9eb648fb7a5f
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-26 00:48:14 -0600

    Rubocop style fixes

--- spec/factories/plants.rb
+++ spec/factories/plants.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
     name { GreekPhilosophers.name }
     frost_date { Faker::Number }
     maturity { Faker::Number.between(from: -10, to: 10) }
-    picture { "MyString" }
   end
 end

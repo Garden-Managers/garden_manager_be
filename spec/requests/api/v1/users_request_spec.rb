ae3a7dc22690b5ea6d9e70eee5c9d93229c5e456
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-26 00:23:34 -0600

    Apply rubocop style changes

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Users API endpoints' do
       create_list(:user, 3)
       get '/api/v1/users'
     end
-    let!(:users) {JSON.parse(response.body, symbolize_names: true)}
+    let!(:users) { JSON.parse(response.body, symbolize_names: true) }
 
     it 'returns successful' do
       expect(response).to be_successful
@@ -38,40 +38,40 @@ RSpec.describe 'Users API endpoints' do
 
   describe 'GET user' do
     before(:each) do
-      user_1 = create(:user)
-      get "/api/v1/user", params: {email: user_1.email}
+      user1 = create(:user)
+      get '/api/v1/user', params: { email: user1.email }
     end
-    let!(:user_1) {JSON.parse(response.body, symbolize_names: true)}
+    let!(:user1) { JSON.parse(response.body, symbolize_names: true) }
 
     it 'returns successful' do
       expect(response).to be_successful
     end
 
     it 'returns correct info' do
-      expect(user_1).to have_key(:data)
-      expect(user_1[:data]).to be_a(Hash)
+      expect(user1).to have_key(:data)
+      expect(user1[:data]).to be_a(Hash)
 
-      expect(user_1[:data]).to have_key(:id)
-      expect(user_1[:data][:id]).to be_a(String)
+      expect(user1[:data]).to have_key(:id)
+      expect(user1[:data][:id]).to be_a(String)
 
-      expect(user_1[:data]).to have_key(:type)
-      expect(user_1[:data][:type]).to eq('user')
+      expect(user1[:data]).to have_key(:type)
+      expect(user1[:data][:type]).to eq('user')
 
-      expect(user_1[:data]).to have_key(:attributes)
-      expect(user_1[:data][:attributes]).to be_a(Hash)
+      expect(user1[:data]).to have_key(:attributes)
+      expect(user1[:data][:attributes]).to be_a(Hash)
 
-      expect(user_1[:data][:attributes]).to have_key(:name)
-      expect(user_1[:data][:attributes][:name]).to be_a(String)
+      expect(user1[:data][:attributes]).to have_key(:name)
+      expect(user1[:data][:attributes][:name]).to be_a(String)
 
-      expect(user_1[:data][:attributes]).to have_key(:email)
-      expect(user_1[:data][:attributes][:email]).to be_a(String)
+      expect(user1[:data][:attributes]).to have_key(:email)
+      expect(user1[:data][:attributes][:email]).to be_a(String)
 
-      expect(user_1[:data][:attributes]).to have_key(:zip)
-      expect(user_1[:data][:attributes][:zip]).to be_a(String)
+      expect(user1[:data][:attributes]).to have_key(:zip)
+      expect(user1[:data][:attributes][:zip]).to be_a(String)
     end
 
     xit 'returns status 404 if user email invalid' do
-      get "/api/v1/users/peacemaker@aol.com"
+      get '/api/v1/users/peacemaker@aol.com'
       expect(response.status).to eq(404)
     end
   end

4935ed486b8d158c918b815ff9371dbf9de84dde
Joel Grant <joelmgrant@me.com>
2022-02-26 17:46:56 -0700

    Remove zip validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,14 +5,13 @@ RSpec.describe User, type: :model do
     it { should have_many(:user_plants) }
     it { should have_many(:plants).through(:user_plants) }
   end
-  
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password_digest) }
     it { should have_secure_password }
-    it { should validate_presence_of(:zip) }
   end
 
   describe 'secure user creation' do

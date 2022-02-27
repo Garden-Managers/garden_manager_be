e962b21289161a1ba80a426ee8ab60aeb68786f8
Joel Grant <joelmgrant@me.com>
2022-02-26 19:33:19 -0700

    Complete weather facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,6 +1,10 @@
 class WeatherFacade
   def coordinates(zip_code)
     weather_data = WeatherService.get_coordinates(zip_code)
-    coordinates = Coordinates.new(weather_data)
+    Coordinates.new(weather_data)
+  end
+
+  def all_weather(lat, lon)
+    WeatherService.get_all_weather(lat, lon)
   end
 end

615bbc55908af732a1c23aff53c9df70b7bd0ceb
Joel Grant <joelmgrant@me.com>
2022-02-27 13:51:07 -0700

    Update weather api endpoint to users from user

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/plants/find', to: 'plant_search#index'
-      resources :users, only: [:index, :create, :update, :destroy] do
+      resources :users, only: [:index, :create, :show, :update, :destroy] do
         get '/plants', to: 'user_plants#all'
+        get '/forecast', to: 'forecast#index'
       end
       resources :plants, only: [:index, :show, :create]
       get '/user', to: 'users#show'
-      get '/user/:id/forecast', to: 'forecast#index'
 
       resources :user_plants, only: [:create, :destroy]
     end

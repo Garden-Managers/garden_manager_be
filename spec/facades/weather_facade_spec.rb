948418aa3393c18afb669b23efb38b70668e82b5
Joel Grant <joelmgrant@me.com>
2022-02-26 20:56:31 -0700

    Skip passing tests for facade

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe WeatherFacade do
   describe '#coordinates' do
-    it 'returns the coordinates based on a zip code' do
+    xit 'returns the coordinates based on a zip code' do
       weather = WeatherFacade.new
       coordinates = weather.coordinates(80223)
 
@@ -14,7 +14,7 @@ RSpec.describe WeatherFacade do
   end
 
   describe '#all_weather' do
-    it 'returns the weather data needed for the users dashboard' do
+    xit 'returns the weather data needed for the users dashboard' do
       weather = WeatherFacade.new
       user = create(:user, zip: "80111", latitude: 80, longitude: 90)
       weather_data = weather.all_weather(user.latitude, user.longitude)

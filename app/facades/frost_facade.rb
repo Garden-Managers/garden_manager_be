4a83a40461f4107ef4ec769ad1af52ab6d11e21f
Joel Grant <joelmgrant@me.com>
2022-02-27 16:44:00 -0700

    Feat: Create frost date API endpoint

--- app/facades/frost_facade.rb
+++ app/facades/frost_facade.rb
@@ -1,12 +1,12 @@
 class FrostFacade
-  def spring_frost_dates(lat, lon)
+  def self.spring_frost_dates(lat, lon)
     weather_station_data = FrostService.find_weather_station_id(lat, lon)
     weather_station_id = weather_station_data.first[:id]
     spring_data = FrostService.get_spring_frost_dates(weather_station_id)
     frost_date = spring_data[1][:prob_50]
   end
 
-  def fall_frost_dates(lat, lon)
+  def self.fall_frost_dates(lat, lon)
     weather_station_data = FrostService.find_weather_station_id(lat, lon)
     weather_station_id = weather_station_data.first[:id]
     fall_data = FrostService.get_fall_frost_dates(weather_station_id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/plants/find', to: 'plant_search#index'
-      resources :users, only: [:index, :create, :update, :destroy] do
+      resources :users, only: [:index, :create, :show, :update, :destroy] do
+        get '/forecast', to: 'forecast#index'
         get '/plants', to: 'user_plants#all'
+        get '/frost_date', to: 'frost#index'
       end
       resources :plants, only: [:index, :show, :create]
       get '/user', to: 'users#show'
-      get '/user/:id/forecast', to: 'forecast#index'
 
       resources :user_plants, only: [:create, :destroy]
     end

e74f6d6cee74a43b5fa2b686f6b7745d78f908b5
Joel Grant <joelmgrant@me.com>
2022-02-28 11:22:51 -0700

    Remove show action for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/plants/find', to: 'plant_search#index'
-      resources :users, only: [:index, :create, :show, :update, :destroy] do
+      resources :users, only: [:index, :create, :update, :destroy] do
         get '/forecast', to: 'forecast#index'
         get '/plants', to: 'user_plants#all'
         get '/frost_date', to: 'frost#index'

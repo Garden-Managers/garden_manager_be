a19294727c36787848da3285d6b975eac53c13fe
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-02-28 22:28:19 -0700

    change routes to search by user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/plants/find', to: 'plant_search#index'
-      resources :users, only: [:index, :create, :update, :destroy] do
+      resources :users, only: [:index, :show, :create, :update, :destroy] do
         get '/plants', to: 'user_plants#all'
       end
       resources :plants, only: [:index, :show, :create]
